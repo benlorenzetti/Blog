1820ee8359eec6d28a7a74c9ca1db2018c5850e5
Ben Lorenzetti <benlorenzetti@DESKTOP-HI69LUG.attlocal.net>
2018-02-03 11:17:13 -0500

    merge from laptop

--- Dual-Heap/test.cpp
+++ Dual-Heap/test.cpp
@@ -5,11 +5,9 @@ using namespace std;
 int main() {
     W_m w1 = {255};
     W_m w2 = {4};
-    W_m w3 = w1 * w2;
-    W_mxm w4(w1, w2);
+    W_mxm w3 = w1 * w2;
     cout << str(w1) << endl;
     cout << str(w2) << endl;
-    cout << str(w3) << endl;
-    cout << str(upperhalf(w4)) << endl;
-    cout << str(lowerhalf(w4)) << endl;
+    cout << str(uhalf(w3)) << endl;
+    cout << str(lhalf(w3)) << endl;
 }
